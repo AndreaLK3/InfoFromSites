5c6455168e8e59fe42a41b47d8f16647b91c2bb2
AndreaLK3 <andrea.lekkas@outlook.com>
2022-04-09 17:20:36 +0200

    =v0.1, using Selenium to get text from dynamic webpages

--- T1.py
+++ T1.py
@@ -2,8 +2,33 @@ import pandas as pd
 import requests
 import Utils
 import logging
-from bs4 import BeautifulSoup
 import re
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+def get_webdriver():
+    # We need a Selenium webdriver to get 100% of the text from dynamic webpages that rely on javascript
+
+    opts = Options()
+    # opts.add_argument(" — headless") # Uncomment if the headless version needed
+    opts.binary_location = "<path to Chrome executable>"
+
+    # # Set the location of the webdriver
+    # chrome_driver = os.getcwd() + "<Chrome webdriver filename>"
+    #
+    # # Instantiate a webdriver
+    # driver = webdriver.Chrome(options=opts, executable_path=chrome_driver)
+
+    # Load the HTML page
+    # driver.get(os.getcwd() + "/test.html")
+    #
+    # # Parse processed webpage with BeautifulSoup
+    # soup = BeautifulSoup(driver.page_source)
+    # print(soup.find(id="test").get_text())
+
 
 def exe():
     companies = pd.read_excel("InputData.xlsx", sheet_name=0)
@@ -14,14 +39,33 @@ def exe():
 
     for i, row in companies.iterrows():
         website_url = row["website"]
-        page_response = requests.get(website_url)
+
+        edge_options = webdriver.EdgeOptions()
+        edge_options.use_chromium = True
+        edge_options.add_argument('headless')
+        edge_options.add_argument('disable-gpu')
+        driver = webdriver.Edge(executable_path='msedgedriver.exe', options=edge_options)
+        driver.get(website_url)
+        page_txt = driver.page_source
         # Steps:
         # gather the subpages of the site at 1 level of depth
         # run a regex search on the site and its subpages for {alphanum}+@{alphanum}+.{alphabet}+
-        links_pt = r'href=[":A-Za-z0-9.\/-]+[.](\w)+'
-        all_links = re.search(links_pt, page_response.text)
-        logging.info(all_links)
-        logging.info(page_response.json())
-        # logging.info(page_response.text)
+        links_pt = re.compile('href=(\S)+\.([\w])+/([a-zA-z0-9-_])+')
+        matches = [xp.group(0) for xp in re.finditer(links_pt, page_txt)]
+        all_links = [s.replace('href="', "") for s in matches]
+        subpage_links = list(set([l for l in all_links if l.startswith(website_url)]))
+        logging.info(website_url + ": " + str([link for link in subpage_links]))
+
+        for subpage_url in subpage_links:
+            driver.get(subpage_url)
+            page_txt = driver.page_source
+            emails_pt =re.compile("([A-Za-z0-_9])+@([A-Za-z0-_9])+(\.[A-Z|a-z]{2,})+")
+            email_addresses = list(set([xp.group(0) for xp in re.finditer(emails_pt, page_txt)]))
+            if len(email_addresses) > 0:
+                logging.info(subpage_url + ": " + str([address for address in email_addresses]))
+
+
+
+
 
     return companies, rounds
