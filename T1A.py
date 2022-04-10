4c16e939a8d2c22ec6999c385d72b7e4d34c438c
AndreaLK3 <andrea.lekkas@outlook.com>
2022-04-10 19:50:53 +0200

    =v0.46, refined the retrieval of emails and phone numbers

--- T1A.py
+++ T1A.py
@@ -22,77 +22,66 @@ def get_webdriver():
     return driver
 
 
+def get_subpage_links(website_url, page_txt):
+    links_pt = re.compile('href='
+                          '((\S)+\.([^\s"])+'  # absolute URL
+                          '|"/([^\s"])+)')  # or: relative URL
+    matches = [xp.group(0) for xp in re.finditer(links_pt, page_txt)]
+    all_links = [s.replace('href="', "") for s in matches]
+    subpage_links = list(set([l for l in all_links if (l.startswith(website_url) or l.startswith("/"))]))
+    extensions_to_exclude = ["xml", "css", "js", "png"]
+    subpage_links = list(filter(lambda l: not(any([(l.endswith(ext)) for ext in extensions_to_exclude])), subpage_links))
+    return subpage_links
+
+
 def retrieve_emails_and_numbers():
     companies = pd.read_excel("InputData.xlsx", sheet_name=0)
     # rounds = pd.read_excel("InputData.xlsx", sheet_name=1)
 
     driver = get_webdriver()
 
-    companies = companies[0:20]
-    Utils.init_logging("Companies.log")
+    # companies = companies[50:51]
+    Utils.init_logging("Info.log")
 
     # output file. Since it takes > 5 minutes, we save the partial results
-    f = open('Info.csv', 'w')
+    f = open('Info.csv', 'w', newline='')
     writer = csv.writer(f)
     writer.writerow(["website","emails", "phone_numbers"])
 
     for i, row in companies.iterrows():
         website_url = row["website"]
+        logging.info("\nPage: " + website_url)
         try:
             driver.get(website_url)
         except Exception as e:
-            logging.warning(website_url)
             logging.warning(e)
             continue
         page_txt = driver.page_source
         # Steps:
         # gather the subpages of the site at 1 level of depth
         # run a regex search on the site and its subpages for {alphanum}+@{alphanum}+.{alphabet}+, and phone numbers
-        links_pt = re.compile('href='
-                              '((\S)+\.([^\s"])+'  # absolute URL
-                              '|"/([^\s"])+)'  # or: relative URL
-                              '(?!\.(.)+)')  # not ending in .xml, .css, or similar
-        matches = [xp.group(0) for xp in re.finditer(links_pt, page_txt)]
-        all_links = [s.replace('href="', "") for s in matches]
-        subpage_links = list(set([l for l in all_links if (l.startswith(website_url) or l.startswith("/"))]))
-        # subpage_links = list(filter(lambda l: not(l.endswith("xml")), potential_subpage_links))
-        logging.info("\nPage: " + website_url)
+        subpage_links = get_subpage_links(website_url, page_txt)
         # logging.info("List of subpages: " + str(subpage_links))
 
         site_emails = set()
         site_phones = set()
-        session = requests.Session()
 
         for subpage_url in subpage_links:
             if subpage_url.startswith(("/")):  # relative URL
                 subpage_url = website_url + subpage_url[1:]
             logging.info("Subpage:" + subpage_url)
             try:
-                # for speed, and because we do not need to follow any dynamic links, use requests to get the static text
-                response = session.get(subpage_url)
+                driver.get(subpage_url)
             except Exception as e:
                 logging.warning(e)
                 continue
-            page_txt = response.text
-            # *** Debug ***
-            # ampersand_pt = re.compile("(.)+@(.+)")
-            # amps = list(set([xp.group(0) for xp in re.finditer(ampersand_pt, page_txt)]))
-            # logging.info("@ found in: " + str(amps))
-            # ***
-            emails_pt = re.compile("(?<=mailto:)?([A-Za-z0-9])+@([A-Za-z0-_9])+(\.[A-Z|a-z]{2,})+")
-            addresses = list(set([xp.group(0) for xp in re.finditer(emails_pt, page_txt)]))
-            #if len(addresses) > 0:
-            #    logging.info("Addresses: " + str(addresses))
+            page_txt = driver.page_source
 
-            not_email_fragments = ["sentry", "wixpress"]
-            # email_addresses_ls = []
+            emails_pt = re.compile("(?<=mailto:)?([A-Za-z0-9])+@([A-Za-z0-_9])+(\.[A-Z|a-z]{2,5})+")
+            addresses = list(set([xp.group(0) for xp in re.finditer(emails_pt, page_txt)]))
+            not_email_fragments = ["sentry", "wixpress", "png", "gif", "jpeg", "example"]  # to exclude loader@2x.gif, etc.
             email_addresses_ls = list(filter(lambda addr:
                                              not(any([fragment in addr for fragment in not_email_fragments])), addresses))
-            # for address in addresses:
-            #     if any([fragment in address for fragment in not_email_fragments]):
-            #         continue
-            #     else:
-            #         email_addresses_ls.append(address)
 
             if len(email_addresses_ls) > 0:
                 logging.info("Subpage: " + subpage_url + " ; E-mails: " + str(email_addresses_ls))
@@ -100,20 +89,21 @@ def retrieve_emails_and_numbers():
             site_emails = site_emails.union(set(email_addresses_ls))
 
             # The phone numbers are taken from the visible text of the HTML pages, thus we need BeautifulSoup
-            soup = bs(response.text, features="lxml")
+            soup = bs(page_txt, features="lxml")
             visible_text = soup.getText(separator=" _ ")
-            numbers_pt = re.compile("(:|\s)+"  # starts with whitespace or directly after :
-                                    "(\+)?"    # the first char may be a +
+            numbers_pt = re.compile("(?<=el|ne)?"  # tel/phone
+                                    "(:|\s)+(\+)?"    # starts with whitespace or directly after :, and maybe a +
                                     "(([0-9]){2,10}(\s)+)+")  # 1 or more sequences of numbers
             numbers = [xp.group(0) for xp in re.finditer(numbers_pt, visible_text)]
             numbers_digits = ["".join(list(filter(lambda c: c in string.digits+"+", num_str))) for num_str in numbers]
             phone_numbers = list(filter(lambda num: 7 <= len(num) <= 14, numbers_digits))  # number length
-            phone_numbers = list(filter(lambda num: not(any([num.count(c) > len(num) / 2 for c in num])),
-                                        phone_numbers)) # not too many identical digits
+            # are phone numbers already present, starting with +? if so, eliminate the others, they're likely not phones
+            if any(["+" in num for num in phone_numbers]):
+                phone_numbers = list(filter(lambda num: "+" in num, phone_numbers))
+
             if len(phone_numbers) > 0:
                 logging.info("Subpage: " + subpage_url + " ; Phone numbers: " + str(phone_numbers))
             site_phones = site_phones.union(set(phone_numbers))
-
 
             time.sleep(1)  # to avoid rate limits on HTTP requests to a website
 
@@ -124,4 +114,14 @@ def retrieve_emails_and_numbers():
     driver.close()
     f.close()
 
-    # Examining the current e-mail results: which sites do not have e-mails, or have questionable e-mails?
+    # Examining the current e-mail and phone results: which sites have missing / questionable elements?
+    # grofit.eu: no phone, confirmed.
+    # app.wasteout.ru : nothing. it's in Russian and most of the site is not accessible
+    # agrionica.com : nothing. Empty page, with a title "insufficient money on the account" in Ciryllic
+    # 5: cyberquant.org: why 3757791 among the phone numbers? because of 3,757,791
+    # 6: plumetec.it: why 2105159? Because of "Numero REA: 2105159"
+    # 7: ilovesnacks.co.uk: why 197624269, 09178679? Because of "Company registration number, VAT number"
+    # 8: tukea.de : no phone. confirmed
+    # 9: birch.earth: no phone. And why did I not get info@birch.earth? because it has 5 letters in its custom domain
+    # circolution.com: why 346035722? Because of "Bel ons: 0341 842121"
+    # innova.co.uk: why '01242388633', '+442045308369', '01242371990', '02037874320', they are actually phone numbers
