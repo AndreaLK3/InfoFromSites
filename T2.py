f60fe5ff90739db2b64c1c53db8574dc39d3e59b
AndreaLK3 <andrea.lekkas@outlook.com>
2022-04-12 20:58:54 +0200

    =...

--- T2.py
+++ T2.py
@@ -17,6 +17,35 @@ from bs4 import BeautifulSoup as bs
 import csv
 
 
+def get_date_pt():
+    """ Get the regex pattern to locate a date in the text"""
+    month_letters = "(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)([a-z])*"
+    dd_mm_numbers = "(\d{1,2})"
+    day_ordinal = "(\d{1,2})(st|nd|rd|th)"
+    year = "(\d{2,4})"
+
+    date_pt = re.compile("(" + dd_mm_numbers+"[/-]" + dd_mm_numbers + "[/-]"+ year +
+                         ")|(" + year + " [/-]" + dd_mm_numbers + "[/-]" + dd_mm_numbers +
+                         ")|(" + month_letters + "( the )*" + day_ordinal + "\s" + year +
+                         ")|(" + day_ordinal + "( of )*" + month_letters + "\s" + year + ")")
+
+    return date_pt
+
+
+def get_money_pts():
+    """The regex for amounts of money, with currency. One for URLs, one for text"""
+    all_currency_symbols = "(CHF|DKK|SEK|NOK|kr|EUR|€|GBP|£|PLN|zł|TRY|UAH|ILS|CAD|CLP|USD|\$|AUD|CNY|¥|HK$|INR|₹|SGD|JPY|" \
+                           "BTC|XBT|₿|ETH|Ξ)"
+    text_money_pt = re.compile('(([0-9.,])+(\s)?' + all_currency_symbols +
+                          "|" + all_currency_symbols + '(\s)?([0-9.,])+)'
+                                                       '([mM]|(\s)(million(s)?|Million(s)?|thousand(s)?))')
+
+    url_money_pt = re.compile("(\d){1,2}(-(\d){1,2})?(-)?([mM](illion)?)")
+
+    return url_money_pt, text_money_pt
+
+
+
 def exe():
     Utils.init_logging("FundingRounds.log")
     rounds_df = pd.read_excel("InputData.xlsx", sheet_name=1)
@@ -28,9 +57,6 @@ def exe():
 
     driver = Utils.get_webdriver()
 
-    all_currency_symbols = "(CHF|DKK|SEK|NOK|kr|EUR|€|GBP|£|PLN|zł|TRY|UAH|ILS|CAD|CLP|USD|\$|AUD|CNY|¥|HK$|INR|₹|SGD|JPY|" \
-                           "BTC|XBT|₿|ETH|Ξ)"
-
     for i, row in rounds_df.iterrows():
         news_url = row["news_url"]
         try:
@@ -41,15 +67,18 @@ def exe():
 
         soup = bs(driver.page_source, features="lxml")
         visible_text = soup.getText(separator=" ")
-        money_pt = re.compile('(([0-9.,])+(\s)?'+all_currency_symbols +
-                              "|"+all_currency_symbols+'(\s)?([0-9.,])+)'
-                               '([mM]|(\s)(million(s)?|Million(s)?|thousand(s)?))')
 
-        all_mentions_of_money = list(set([mtc.group(0).lower() for mtc in re.finditer(money_pt, visible_text)]))
+        url_money_pt, text_money_pt = get_money_pts()
+
+
+        all_mentions_of_money = list(set([mtc.group(0).lower() for mtc in re.finditer(text_money_pt, visible_text)]))
         if len(all_mentions_of_money) > 1:
-            all_mentions_of_money = all_mentions_of_money[0:1]
-        logging.info((row["news_url"], str(all_mentions_of_money)))
-        writer.writerow([row["client_id"], row["news_url"], str(all_mentions_of_money)])
+            funding = all_mentions_of_money[0]  # get the first. The next ones may be total funding, investor etc.
+        else:  # we did not find it in the text. Maybe the URL has it
+            funding = "not found"
+
+        logging.info((row["news_url"], str(funding)))
+        writer.writerow([row["client_id"], row["news_url"], str(funding)])
 
 
 
